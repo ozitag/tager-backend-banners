e768eb19ac5e683f3b7d47c9d1a8534f0575f538
Vital Ozierski <v.ozierski@ozitag.com>
2020-07-17 17:39:21 +0300

    change 'banners' to 'market-boards' by the adblock

--- routes/routes.php
+++ routes/routes.php
@@ -2,21 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/tager/banners/{alias}', \OZiTAG\Tager\Backend\Banners\Controllers\PublicController::class . '@banner');
+Route::get('/tager/market-boards/{alias}', \OZiTAG\Tager\Backend\Banners\Controllers\PublicController::class . '@banner');
 
 Route::group(['prefix' => 'admin', 'middleware' => ['passport:administrators', 'auth:api']], function () {
-    Route::get('/banners', \OZiTAG\Tager\Backend\Banners\Controllers\AdminController::class . '@index');
-    Route::post('/banners', \OZiTAG\Tager\Backend\Banners\Controllers\AdminController::class . '@create');
-    Route::get('/banners/{id}', \OZiTAG\Tager\Backend\Banners\Controllers\AdminController::class . '@view');
-    Route::get('/banners/{alias}', \OZiTAG\Tager\Backend\Banners\Controllers\AdminController::class . '@viewByAlias');
-    Route::put('/banners/{id}', \OZiTAG\Tager\Backend\Banners\Controllers\AdminController::class . '@update');
-    Route::delete('/banners/{id}', \OZiTAG\Tager\Backend\Banners\Controllers\AdminController::class . '@delete');
+    Route::get('/market-boards', \OZiTAG\Tager\Backend\Banners\Controllers\AdminController::class . '@index');
+    Route::post('/market-boards', \OZiTAG\Tager\Backend\Banners\Controllers\AdminController::class . '@create');
+    Route::get('/market-boards/{id}', \OZiTAG\Tager\Backend\Banners\Controllers\AdminController::class . '@view');
+    Route::get('/market-boards/{alias}', \OZiTAG\Tager\Backend\Banners\Controllers\AdminController::class . '@viewByAlias');
+    Route::put('/market-boards/{id}', \OZiTAG\Tager\Backend\Banners\Controllers\AdminController::class . '@update');
+    Route::delete('/market-boards/{id}', \OZiTAG\Tager\Backend\Banners\Controllers\AdminController::class . '@delete');
 
-    Route::get('/banners/{id}/items', \OZiTAG\Tager\Backend\Banners\Controllers\AdminController::class . '@listItems');
-    Route::post('/banners/{id}/items', \OZiTAG\Tager\Backend\Banners\Controllers\AdminController::class . '@createItem');
-    Route::get('/banners/items/{id}', \OZiTAG\Tager\Backend\Banners\Controllers\AdminController::class . '@viewItem');
-    Route::put('/banners/items/{id}', \OZiTAG\Tager\Backend\Banners\Controllers\AdminController::class . '@updateItem');
-    Route::delete('/banners/items/{id}', \OZiTAG\Tager\Backend\Banners\Controllers\AdminController::class . '@removeItem');
-    Route::post('/banners/items/{id}/{direction}', \OZiTAG\Tager\Backend\Banners\Controllers\AdminController::class . '@moveItem')
+    Route::get('/market-boards/{id}/items', \OZiTAG\Tager\Backend\Banners\Controllers\AdminController::class . '@listItems');
+    Route::post('/market-boards/{id}/items', \OZiTAG\Tager\Backend\Banners\Controllers\AdminController::class . '@createItem');
+    Route::get('/market-boards/items/{id}', \OZiTAG\Tager\Backend\Banners\Controllers\AdminController::class . '@viewItem');
+    Route::put('/market-boards/items/{id}', \OZiTAG\Tager\Backend\Banners\Controllers\AdminController::class . '@updateItem');
+    Route::delete('/market-boards/items/{id}', \OZiTAG\Tager\Backend\Banners\Controllers\AdminController::class . '@removeItem');
+    Route::post('/market-boards/items/{id}/{direction}', \OZiTAG\Tager\Backend\Banners\Controllers\AdminController::class . '@moveItem')
         ->where('direction', 'up|down');
 });
